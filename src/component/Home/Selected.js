48d5f3b3f4be98832a86bfd1c6f4c22b9a3da31b
Nguyen Long <long.doan.180021@student.fulbright.edu.vn>
2021-12-12 12:11:11 +0700

    small fix on pop-up header

--- src/component/Home/Selected.js
+++ src/component/Home/Selected.js
@@ -45,9 +45,11 @@ export default function Selected(props) {
         <div className={"fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto " + (isOpen ? "" : "hidden")}>
             <div className="relative my-auto mx-auto p-5 border w-7/8 h-screen shadow-lg rounded-md bg-white">
                 <p className='text-5xl text-right mr-4'>
-                    <span className='text-gold'>Hôm nay</span>
-                    <span className='text-blue'>, bạn có thể thử</span>
-                    <div className="modal-close cursor-pointer inline-block ml-64" onClick={() => {toggleOpen(false); dispatch({type: "RESET_SELECTION"})}}>
+                    <div className='mr-32 inline-block'>
+                        <span className='text-gold'>Hôm nay</span>
+                        <span className='text-blue'>, bạn có thể thử</span>
+                    </div>
+                    <div className="modal-close cursor-pointer inline-block ml-64 mr-4" onClick={() => {toggleOpen(false); dispatch({type: "RESET_SELECTION"})}}>
                         <svg className="fill-current text-gold border" xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 18 18">
                             <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z" />
                         </svg>

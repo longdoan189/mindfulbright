72933af9e5e4000fef5fcae56f4ded1660ab8d89
Nguyen Long <long.doan.180021@student.fulbright.edu.vn>
2021-12-16 20:13:58 +0700

    minor style fix

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -22,7 +22,7 @@ export default function Home() {
             <div className='grid sm:grid-cols-2'>
                 <div className='ml-2 sm:ml-24 lg:ml-40 lg:mt-20'>
                     <h1 className='text-blue text-center sm:text-left text-3xl lg:text-5xl font-bold'>Stay Healthy & <br/> Be Present</h1>
-                    <p className='mt-8 text-black text-left text-lg font-bold'>MindFulbright là một không gian để chữa lành và thư giãn, thông qua việc thực hành Chánh niệm. Với những video và podcast hướng dẫn, bé Chani sẽ cùng bạn lần lượt đi qua các bài tập giúp cải thiện sức khoẻ tinh thần của bạn. </p>
+                    <p className='mt-8 text-black text-left text-lg'>MindFulbright là một không gian để chữa lành và thư giãn, thông qua việc thực hành Chánh niệm. Với những video và podcast hướng dẫn, bé Chani sẽ cùng bạn lần lượt đi qua các bài tập giúp cải thiện sức khoẻ tinh thần của bạn. </p>
                     <button className="text-sm md:text-lg mt-5 background-gold hover:text-yellow-500 px-3 py-3 border rounded-lg mx-auto block" onClick={() => {setOpenModal(true)}}><b>Tìm hiểu thêm</b></button>
                 </div>
                 <div className='ml-16 lg:ml-32 mr-12 mt-12'>

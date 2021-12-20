d90f4b72ca95cc745065ab9924b2c7e95f9650f6
Nguyen Long <long.doan.180021@student.fulbright.edu.vn>
2021-12-20 17:04:47 +0700

    add warning for browser block and add audio feature

--- src/component/ExerciseList/SingleExercise.js
+++ src/component/ExerciseList/SingleExercise.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {NavLink, useLocation} from "react-router-dom";
 import DATA from '../../data/post.json'
 import './SingleExercise.css'
@@ -7,6 +7,19 @@ export default function Exercise(props) {
     const {pathname} = useLocation();
     const id = pathname.split("/").slice(-1)[0];
     const curExercise = DATA.filter(ex => {return ex.id === id})[0];
+
+    const [isShowLink, setShowLink] = useState(false)
+    const showLink = (isShow, type) => {
+        if (isShow && type==="Video") {
+            return "Bạn có thể đang sử dụng tính năng chặn quảng cáo hoặc chặn theo dõi (ví dụ AdBlock hoặc Edge ở chế độ Bảo mật). Bạn có thể thử tắt nó, sau đó tải lại trang. Nếu bạn không muốn tắt nó, bạn có thể truy cập trực tiếp tại nút 'pop' ở góc trên cùng bên phải của video"
+        }
+        if (isShow && type==="Podcast") {
+            return "Tính năng nghe podcast đang được xây dựng nên có thể lỗi ở một số nền tảng. Bạn có thể thử nghe bằng video nhé!"
+        }
+        else {
+            return ""
+        }
+    }
     const renderContent = () => {
         let frame;
         if (curExercise.content === null || curExercise.content === "" || curExercise.content===undefined) {
@@ -29,8 +42,24 @@ export default function Exercise(props) {
                 frame = "container-iframe-16-9"
             }
             return (
-                <div className={"my-8 mx-auto " + frame}>
-                    <iframe src={curExercise.content} frameBorder="0" allowFullScreen={true} webkitallowfullscreen="true" mozallowfullscreen="true" title="video" autoPlay="1" className="responsive-iframe"></iframe>
+                <div className="w-100 my-8">
+                    <p className="text-center text-gold cursor-pointer" onClick={() => {setShowLink(!isShowLink)}}>Không xem được video này?</p>
+                    <p className="text-center text-blue mx-8 sm:mx-32 lg:mx-64">{showLink(isShowLink, curExercise.type)}</p>
+                    <div className={"mx-auto " + frame}>
+                        <iframe src={curExercise.content} frameBorder="0" allowFullScreen={true} webkitallowfullscreen="true" mozallowfullscreen="true" title="video" autoPlay="1" className="responsive-iframe"></iframe>
+                    </div>
+                </div>
+            )
+        }
+
+        if(curExercise.type==="Podcast") {
+            return (
+                <div className="w-100 my-8">
+                    <p className="text-center text-gold cursor-pointer" onClick={() => {setShowLink(!isShowLink)}}>Không nghe được podcast này?</p>
+                    <p className="text-center text-blue mx-8 sm:mx-32 lg:mx-64">{showLink(isShowLink, curExercise.type)}</p>
+                    <div className={"mx-auto " + frame}>
+                        <iframe src={curExercise.content} frameBorder="0" allowFullScreen={true} webkitallowfullscreen="true" mozallowfullscreen="true" title="video" autoPlay="1" className="responsive-iframe" style={{"zIndex": "-10"}}></iframe>
+                    </div>
                 </div>
             )
         }
@@ -44,7 +73,7 @@ export default function Exercise(props) {
 
     return (
         <div className="w-100">
-            <div className="grid grid-cols-3">
+            <div className="lg:grid lg:grid-cols-3">
                 <button className="text-sm md:text-lg background-gold hover:text-yellow-500 px-3 py-3 border rounded-lg mx-auto block">
                     <NavLink to="/instruction">
                         <b>Xem các bài tập khác</b>

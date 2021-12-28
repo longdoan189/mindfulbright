2ccaed36d74239b888093d549fb7cfcc926c96d5
Nguyen Long <long.doan.180021@student.fulbright.edu.vn>
2021-12-28 21:40:54 +0700

    fix pop-up design error

--- src/component/ExerciseList/ExerciseList.js
+++ src/component/ExerciseList/ExerciseList.js
@@ -15,16 +15,16 @@ export default function ExerciseList(props) {
                             className="w-full h-64 rounded-r object-cover"
                         />
                         <div className='sm:col-span-4 lg:col-span-9'>
-                            <h3 className="text-blue text-center text-xl lg:text-2xl font-blog py-2">
+                            <h3 className="text-blue text-center text-base lg:text-xl xl:text-2xl font-blog py-2">
                                 <b>{filtered_post.name}</b>
                             </h3>
-                            <div className='flex secondary-font text-center text-xs sm:text-base lg:text-lg font-blog py-2 h-32 lg:h-64 xl:h-48 '>
-                                <p className='my-auto'>{filtered_post.description.slice(0,250)}</p>
+                            <div className='flex secondary-font text-center text-xs lg:text-base xl:text-lg font-blog py-2 h-32 lg:h-64 xl:h-48 '>
+                                <p className='my-auto'>{filtered_post.description.slice(0,250)}...</p>
                             </div>
                         </div>
                         
                         <NavLink to={`/instruction/${filtered_post.id}`}>
-                            <button className="text-sm md:text-lg mt-5 background-gold hover:text-yellow-500 px-3 py-3 border rounded-lg mx-auto block">
+                            <button className="text-sm md:text-lg mt-4 sm:mt-8 lg:mt-12 background-gold hover:text-yellow-500 px-3 py-3 border rounded-lg mx-auto block">
                                 <b>Xem bài tập</b>
                             </button>
                         </NavLink>
--- src/component/Home/Selected.js
+++ src/component/Home/Selected.js
@@ -39,13 +39,13 @@ export default function Selected(props) {
     }
     return (
         <div className={"fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto " + (isOpen ? "" : "hidden")}>
-            <div className="relative my-auto mx-auto p-5 border w-2/3 h-screen shadow-lg rounded-md bg-white z-10">
-                <div className='text-xl sm:text-3xl lg:text-5xl text-right mr-4'>
-                    <div className='mr-16 lg:mr-32 inline-block'>
+            <div className="relative my-auto mx-auto p-5 border sm:w-2/3 h-screen shadow-lg rounded-md bg-white z-10">
+                <div className='text-xl sm:text-2xl lg:text-5xl text-right mr-4'>
+                    <div className='inline-block'>
                         <span className='text-gold'>Hôm nay</span>
                         <span className='text-blue'>, bạn có thể thử</span>
                     </div>
-                    <div className="modal-close cursor-pointer inline-block ml-20 sm:ml-40 xl:ml-64 mr-4" onClick={() => {toggleOpen(false); dispatch({type: "RESET_SELECTION"})}}>
+                    <div className="modal-close cursor-pointer inline-block lg:ml-20 mr-4" onClick={() => {toggleOpen(false); dispatch({type: "RESET_SELECTION"})}}>
                         <svg className="fill-current text-gold border" xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 18 18">
                             <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z" />
                         </svg>
